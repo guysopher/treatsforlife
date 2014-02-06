22a5f07169975272d1ac71c4167cae9f3e64610d
Jonathan Bachman <yoni@moment.me>
2014-02-06 12:27:42 +0200

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Treatsforlife::Application.config.secret_token = '8bf5494302954e4c8206c94dabab959b86315acf99e6aec3cd2290178919437948f79c814b85ed46780bcf1936dae89696aa7606077a7db2931148a9e06298a1'
