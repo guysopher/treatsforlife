9fdbd70b9efbaa7923e2a23bfb391ce4787a7e3b
Guy Sopher <guysopher@gmail.com>
2014-02-07 10:47:05 +0200

    css

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -72,5 +72,81 @@ class ApplicationController < ActionController::Base
   end
   helper_method :send_sms
 
+  def pushNotification(user, msgType, ticker, title, message, link=nil, ref=nil)
+    begin
+      userTokensApple=Set.new
+      userTokensAppleTestDev=Set.new
+      userTokensGoogleSet=Set.new
+
+      if (user["push_tokens"])
+        user["push_tokens"].each do |uuid, device_token|
+          if (device_token && device_token["token"])
+            if (device_token["token_type"]=="google" || device_token["token_type"]=='')
+              userTokensGoogleSet << device_token["token"]
+            else #apple
+              if ((device_token["device_type"] || '').start_with?('TESTDEV'))
+                userTokensAppleTestDev << device_token["token"]
+              else
+                userTokensApple << device_token["token"]
+              end
+            end
+          end
+        end
+      end
+
+      userTokensGoogleSet = ''
+
+      urlDestination = 'momentme://'
+
+      userTokensGoogle=userTokensGoogleSet.to_a
+
+      #google
+      sent = false
+      if (userTokensGoogle.length>0)
+        header={"Authorization" => "key=AIzaSyDPMbQU7OdLJg4oBEuHEl83KtUEvsNn-NQ", "Content-Type" => "application/json"}
+        uri="https://android.googleapis.com/gcm/send"
+        body={"data" => {"ticker" => ticker, "title" => title, "msg" => message}, #,"req_id"=>7,"not_id"=>17
+              "registration_ids" => userTokensGoogle, "collapse_key" => msgType}
+
+        body["data"]["url"]=urlDestination
+
+        response=postItStr(uri, body.to_json, header)
+        if (response && response.body)
+          jsonRes=(response.body)
+          if (jsonRes && jsonRes["results"] && jsonRes["results"].length>0)
+            jsonRes["results"].each_with_index do |res, i|
+              if (res['message_id'])
+                if (res["registration_id"])
+                  #the message was pushed to a different ID, so replace it in the DB
+                  tokenToRemove=userTokensGoogle[i]
+                  tokenToAdd=res["registration_id"]
+                end
+
+                $Logger.debug("Push sent succesfully")
+                #send to all devices for this user => return true
+                sent = true
+              elsif (res['error'])
+                $Logger.warn("bad result for push #{res['error']}")
+                if (res['error']=="NotRegistered" || res["error"]=="MismatchSenderId")
+                  tokenToRemove=userTokensGoogle[i]
+                  @usersApi.replaceAppTokenForUser(user, tokenToRemove, nil, Time.new)
+                end
+              elsif (res["registration_id"])
+                # If set, means that GCM processed the message but it has another canonical registration ID for that device, so sender should replace the IDs on future requests (otherwise they might be rejected). This field is never set if there is an error in the request.
+              else
+                $Logger.error("unknown result for Android push notification: #{res.inspect}")
+              end
+            end
+          end
+        end
+      end
+
+      return sent
+    rescue
+      $Logger.error("Error while pushing notifications #{$!.inspect} #{$@.to_s}")
+      return false
+    end
+  end
+
 
 end
