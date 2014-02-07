bf617ca19c18cc88c361e419a9947312719ae7f7
Guy Sopher <guysopher@gmail.com>
2014-02-07 11:20:37 +0200

    nug in paypal

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,11 +1,12 @@
 module ApplicationHelper
 
   def app_url
-    if Rails.env.isProduction?
-      return 'http://treatsforlife.org'
-    else
-      return 'http://localhost:3000'
-    end
+    return 'http://treatsforlife.org'
+    #if Rails.env.isProduction?
+    #  return 'http://treatsforlife.org'
+    #else
+    #  return 'http://localhost:3000'
+    #end
   end
 
 end
